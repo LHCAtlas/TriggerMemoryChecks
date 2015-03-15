fbd5c8b35fc316f9fa5dbff40fe6c841c4bbe3e5
Gordon Watts <gordon@gordonandpaula.net>
2015-03-15 15:07:37 +0100

    Test second step only

--- trigger-phases.sh
+++ trigger-phases.sh
@@ -28,19 +28,19 @@ echo = RDO to ESD
 echo =
 echo ==============================
 echo == - Config Only
-Reco_tf.py --inputRDOFile=/afs/cern.ch/work/l/limosani/public/valid1.117050.PowhegPythia_P2011C_ttbar.recon.RDO.e2658_s1967_s1964_r5787_tid01572821_00/RDO.01572821._000019.pool.root.1  --outputESDFile=myESD.pool.root --preInclude=RecExCommon/ValgrindTweaks.py --maxEvents=${events} --preExec='rec.doTrigger=True;rec.doMonitoring=True' --execOnly  --athenaopts='--config-only=rec.pkl --stdcmalloc' 
+#Reco_tf.py --inputRDOFile=/afs/cern.ch/work/l/limosani/public/valid1.117050.PowhegPythia_P2011C_ttbar.recon.RDO.e2658_s1967_s1964_r5787_tid01572821_00/RDO.01572821._000019.pool.root.1  --outputESDFile=myESD.pool.root --preInclude=RecExCommon/ValgrindTweaks.py --maxEvents=${events} --preExec='rec.doTrigger=True;rec.doMonitoring=True' --execOnly  --athenaopts='--config-only=rec.pkl --stdcmalloc' 
 
 echo == - Valgrind Run
-valgrind --tool=memcheck --leak-check=full --suppressions=root.supp --suppressions=${ROOTSYS}/etc/valgrind-root.supp --suppressions=newSuppressions.supp --suppressions=oracleDB.supp --suppressions=valgrindRTT.supp --suppressions=Gaudi.supp --suppressions=valgrind-python.supp --num-callers=30 `which python` `which athena.py` rec.pkl >& valgrind.rdotoesd.${mydesc}.log
+#valgrind --tool=memcheck --leak-check=full --suppressions=root.supp --suppressions=${ROOTSYS}/etc/valgrind-root.supp --suppressions=newSuppressions.supp --suppressions=oracleDB.supp --suppressions=valgrindRTT.supp --suppressions=Gaudi.supp --suppressions=valgrind-python.supp --num-callers=30 `which python` `which athena.py` rec.pkl >& valgrind.rdotoesd.${mydesc}.log
 
 # ESD TO AOD STEP (with trigger)
 echo ==============================
 echo =
-echo = ESD to ADO
+echo = ESD to AOD
 echo =
 echo ==============================
 echo == - Prep
-Reco_tf.py --inputRDOFile=/afs/cern.ch/work/l/limosani/public/valid1.117050.PowhegPythia_P2011C_ttbar.recon.RDO.e2658_s1967_s1964_r5787_tid01572821_00/RDO.01572821._000019.pool.root.1 --outputESDFile=myESD.pool.root --maxEvents=${events} --preExec='rec.doTrigger=True;rec.doMonitoring=True' 
+Reco_tf.py --inputRDOFile=/afs/cern.ch/work/l/limosani/public/valid1.117050.PowhegPythia_P2011C_ttbar.recon.RDO.e2658_s1967_s1964_r5787_tid01572821_00/RDO.01572821._000019.pool.root.1 --outputESDFile=myESD.pool.root --maxEvents=${events} --preExec='rec.doTrigger=True;rec.doMonitoring=False' 
 
 echo == - Config
 Reco_tf.py --inputESDFile=myESD.pool.root --outputAODFile=myAOD.pool.root --maxEvents=${events} --preInclude=RecExCommon/ValgrindTweaks.py --maxEvents=${events} --execOnly  --athenaopts='--config-only=rec.pkl --stdcmalloc' 
