47549867572b96171734b821daccba9a8175b8f9
Gordon Watts <gordon@gordonandpaula.net>
2015-03-28 18:59:52 +0100

    Better memory supressions support

--- trigger-rdotoesd.sh
+++ trigger-rdotoesd.sh
@@ -14,12 +14,19 @@ export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup && source /afs/cer
 # Get suppressions to try to clean up the valgrind file a little bit
 # From http://acode-browser.usatlas.bnl.gov/lxr/source/atlas/Tools/ValgrindRTTJobs/scripts/valgrind_trf.py
 
-get_files newSuppressions.supp
-get_files oracleDB.supp
+#get_files newSuppressions.supp
+#get_files oracleDB.supp
 get_files root.supp
-get_files valgrindRTT.supp
+#get_files valgrindRTT.supp
 get_files Gaudi.supp
 get_files valgrind-python.supp
+
+cp /afs/cern.ch/user/k/krasznaa/public/valgrind/newSuppressions.supp .
+cp /afs/cern.ch/user/k/krasznaa/public/valgrind/oracleDB.supp .
+cp /afs/cern.ch/user/k/krasznaa/public/valgrind/valgrindRTT.supp .
+
+cp /afs/cern.ch/user/k/krasznaa/public/valgrind/pythonJobO.supp .
+cp /afs/cern.ch/user/k/krasznaa/public/valgrind/reflexPyROOT.supp .
 
 # RDO TO ESD STEP
 echo ==============================
@@ -31,7 +38,7 @@ echo == - Config Only
 Reco_tf.py --inputRDOFile=/afs/cern.ch/work/l/limosani/public/valid1.117050.PowhegPythia_P2011C_ttbar.recon.RDO.e2658_s1967_s1964_r5787_tid01572821_00/RDO.01572821._000019.pool.root.1  --outputESDFile=myESD.pool.root --preInclude=RecExCommon/ValgrindTweaks.py --maxEvents=${events} --preExec='rec.doTrigger=True;rec.doMonitoring=True' --execOnly  --athenaopts='--config-only=rec.pkl --stdcmalloc' 
 
 echo == - Valgrind Run
-valgrind --tool=memcheck --leak-check=full --suppressions=root.supp --suppressions=${ROOTSYS}/etc/valgrind-root.supp --suppressions=newSuppressions.supp --suppressions=oracleDB.supp --suppressions=valgrindRTT.supp --suppressions=Gaudi.supp --suppressions=valgrind-python.supp --num-callers=30 `which python` `which athena.py` rec.pkl >& valgrind.rdotoesd.${mydesc}.log
+valgrind --tool=memcheck --leak-check=full --suppressions=root.supp --suppressions=${ROOTSYS}/etc/valgrind-root.supp --suppressions=newSuppressions.supp --suppressions=oracleDB.supp --suppressions=valgrindRTT.supp --suppressions=Gaudi.supp --suppressions=valgrind-python.supp --supressions=pythonJobO.supp --supressions=reflexPyROOT.supp --num-callers=30 `which python` `which athena.py` rec.pkl >& valgrind.rdotoesd.${mydesc}.log
 
 gzip valgrind.rdotoesd.${mydesc}.log
 
